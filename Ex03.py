08e8b82ba450597b84495460df1e333f647d5b8d
naviat <haidv.ict@gmail.com>
2017-06-12 14:26:54 +0700

    commit for Ex03

---
+++ Ex03.py
@@ -0,0 +1,20 @@
+print("I will now count my chickens:")
+print("Hens ", 25 + 30  / 6)
+print("Rootser", 100 - 25 * 3 / 4)
+
+print("Now i will count the eggs: ")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+
+print("It is true that 3 + 2 < 5 - 7 ?")
+print(3 +  2 < 5 - 7)
+
+print("What is 3 + 2 ?", 3 + 2)
+print("What 0s 5- 7 ?", 5 - 7)
+
+print("Oh that is why it's FALSE")
+
+print("How about somemore? ")
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
