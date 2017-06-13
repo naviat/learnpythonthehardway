a70ab7879664f0e82639e71be039d863e23ad191
naviat <haidv.ict@gmail.com>
2017-06-13 11:55:10 +0700

    rewrite all

--- Ex06.py
+++ Ex06.py
@@ -5,7 +5,7 @@ import sys
 x = "There are %d types of people" % 10
 binary = "binary"
 do_not = "don't"
-y = "Those who know %s and those who %s." %(binary,do_not)
+y = "Those who know %s and those who %s." % (binary,do_not)
 
 print(x)
 print(y)
