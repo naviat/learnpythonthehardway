974d8706126dae6c3113ee3aa3bb10fa1e428219
naviat <haidv.ict@gmail.com>
2017-06-13 17:23:50 +0700

    update 13062017-01

---
+++ Ex10.py
@@ -0,0 +1,15 @@
+tabby_cat = "\vI'm tabbed in."
+persian_cat = "\vI'm split\n\von a line."
+backsplash_cat = "\vI'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backsplash_cat)
+print(fat_cat)
