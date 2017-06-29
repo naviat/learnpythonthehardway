299cd91694fe1a436337845b79b599dcb50775d0
Hải Đàm <haidv.ict@hotmail.com>
2017-06-29 14:22:43 +0700

    update form my laptop

---
+++ Ex13.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+#script, first, second, third = argv
+script = argv
+first = input("Your first variable ")
+second = input("Your first variable ")
+third = input("Your first variable ")
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
